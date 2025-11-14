0424367fc0c19519fd60b92553b31c7975875444
vincent-guizot <vincentguizot128@gmail.com>
2025-11-14 21:42:06 +0700

    basic dart

---
+++ w1/day1-basic.dart
@@ -0,0 +1,15 @@
+void main() {
+  int number = 10;
+  String str = "code";
+
+  const pi = 3.14;
+  final time = DateTime.now();
+
+  bool isAvailable = true;
+
+  print(number);
+  print(str);
+  print(pi);
+  print(time);
+  print(isAvailable);
+}
